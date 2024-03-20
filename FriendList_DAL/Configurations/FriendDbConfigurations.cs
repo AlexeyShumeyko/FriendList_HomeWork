569b8c15b5a5e6bb8fbc366dc5effce43d2b7c4e
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-03-20 21:15:50 +0300

    Create DBContext; add configuration for Friend

---
+++ FriendList_DAL/Configurations/FriendDbConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using FriendList_DomainModel.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendList_DAL.Configurations
+{
+    internal class FriendDbConfigurations : IEntityTypeConfiguration<Friend>
+    {
+        public void Configure(EntityTypeBuilder<Friend> builder)
+        {
+        }
+    }
+}
---
+++ FriendList_DAL/FriendListDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FriendList_DomainModel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendList_DAL
+{
+    public class FriendListDbContext : DbContext
+    {
+        public FriendListDbContext(DbContextOptions<FriendListDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Friend> Friends { get; set; }
+    }
+}
