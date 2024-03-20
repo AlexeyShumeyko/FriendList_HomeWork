0313a6918433b36ece90fd82f7f697c972c498c5
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-03-20 21:34:33 +0300

    Creating a controller and view for entity Friend; Setting up home page

---
+++ FriendList/Controllers/FriendController.cs
@@ -0,0 +1,37 @@
+﻿using FriendList_DomainModel.ViewModel;
+using FriendList_Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendList.Controllers
+{
+    public class FriendController : Controller
+    {
+        private readonly IFriendService _friendService;
+
+        public FriendController(IFriendService friendService)
+        {
+            _friendService = friendService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var friendsTable = _friendService.GetFriends();
+
+            return View(friendsTable);
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateFriendViewModel model)
+        {
+            await _friendService.Create(model);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
